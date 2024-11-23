43c5f0d77fc21c38ff02cac36069b0d991abe3e0
Mehran Sarmadi <128898167+mehran-sarmadi@users.noreply.github.com>
2024-11-23 19:44:53 +0330

    Update distiller.py

--- distiller.py
+++ distiller.py
@@ -102,8 +102,8 @@ class Distiller(nn.Module):
         if self.args.both_simmam_loss is not None:
             t_simam = self.simam(t_feats[-1])
             s_simam = self.simam(s_feats[-1])
-            t_simam_3 = self.simam(t_feats[-3])
-            s_simam_3 = self.simam(self.Connectors[-3](s_feats[-3]))
+            t_simam_3 = self.simam(t_feats[-2])
+            s_simam_3 = self.simam(self.Connectors[-3](s_feats[-2]))
             b,c,h,w = t_simam.shape
             both_simmam_loss += (s_simam / torch.norm(s_simam, p = 2) - t_simam / torch.norm(t_simam, p = 2)).pow(2).sum() / (b)
             b,c,h,w = t_simam_3.shape
